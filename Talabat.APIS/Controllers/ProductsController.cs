d3b541c749e48ec68c3262678aa5f2ca6b862322
Jemmyx10 <ahmedkhaled910000@gmail.com>
2024-04-20 05:35:11 +0200

    IEnumerable Vs IReadOnlyList

--- Talabat.APIS/Controllers/ProductsController.cs
+++ Talabat.APIS/Controllers/ProductsController.cs
@@ -31,13 +31,13 @@ namespace Talabat.APIS.Controllers
 
         // /api/Products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductWithBrandAndCategorySpecifications();
 
             var products = await _productsRepo.GetAllWithSpecAsync(spec);
 
-            return Ok(_mapper.Map<IEnumerable<Product>, IEnumerable<ProductToReturnDto>>(products));
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [ProducesResponseType(typeof(ProductToReturnDto), StatusCodes.Status200OK)]
@@ -57,14 +57,14 @@ namespace Talabat.APIS.Controllers
         }
 
         [HttpGet("brands")] // Get: /api/products/brands
-        public async Task<ActionResult<IEnumerable<ProductBrand>>> GetBrands()
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetBrands()
         {
             var brands = await _brandsRepo.GetAllAsync();
             return Ok(brands);
         }
 
         [HttpGet("categories")] // Get: /api/products/categories
-        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetCategories()
+        public async Task<ActionResult<IReadOnlyList<ProductCategory>>> GetCategories()
         {
             var categories = await _categoriesRepo.GetAllAsync();
             return Ok(categories);
--- Talabat.Core/Repositories.Contract/IGenericRepository.cs
+++ Talabat.Core/Repositories.Contract/IGenericRepository.cs
@@ -11,13 +11,13 @@ namespace Talabat.Core.Repositories.Contract
     public interface IGenericRepository<T> where T : BaseEntity
     {
 
-        Task<IEnumerable<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllAsync();
 
 
         Task<T?> GetAsync(int id);
 
 
-        Task<IEnumerable<T>> GetAllWithSpecAsync(ISpecifications<T> spec);
+        Task<IReadOnlyList<T>> GetAllWithSpecAsync(ISpecifications<T> spec);
 
 
         Task<T?> GetWithSpecAsync(ISpecifications<T> spec);
